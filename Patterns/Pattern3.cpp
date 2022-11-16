ea91bcc6155153487b4b908de599835915d7ab35
Onkar Karale <onkarkarale4@gmail.com>
2022-11-16 23:21:44 +0530

    Left Triangle Pattern

---
+++ Patterns/Pattern3.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+ 
+
+int main()
+{
+
+  for(int i = 1; i <= 5; i++)
+  {
+    for(int j = 1; j <= i; j++)
+    {
+      cout << " * ";
+   
+    }
+    cout<<"\n";
+  }
+     
+  return 0;
+}
