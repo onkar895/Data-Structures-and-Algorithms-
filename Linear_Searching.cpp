b702087dd02332109bdb8447d50214923eabc96e
Onkar Karale <onkarkarale4@gmail.com>
2022-11-18 20:28:44 +0530

    Linear Search to find the element in the array

---
+++ Linear_Searching.cpp
@@ -0,0 +1,49 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,num1,i;
+    cout<<"Enter the size of an array"<<endl;
+    cin>>num;
+    int arr[num];
+    
+    cout<<"Enter the elements of an array"<<endl;
+    for(i=0; i<num; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Array Elements are:";
+    for (i = 0; i<num; i++)
+    {
+        cout<<arr[i]<<" ";
+       
+    }
+     cout<<"\n";
+    
+    cout<<"Enter the element you want to search for: "<<endl;
+    cin>>num1;
+    for(i=0; i<num; i++)
+    {
+        if(arr[i]==num1)
+        {
+            cout<<"The element "<<num1<<" is present at index "<<i;
+            break;
+        }
+        
+    }
+        if (i == num)
+        {
+            cout<<"The element "<<num1<<" is not present in the array";
+        }
+         
+    return 0;
+}
