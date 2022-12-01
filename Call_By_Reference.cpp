dd7a4d847c7b44c5cc80c5004346b351adc17ad0
Onkar Karale <onkarkarale4@gmail.com>
2022-12-01 16:44:53 +0530

    program to swap numbers by call by reference

---
+++ Call_By_Reference.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+// CALL BY REFERENCE
+
+#include <iostream>
+
+using namespace std;
+
+void swap(int *x, int *y)
+{
+    // Local Variables
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+    
+    cout<<"x:"<<*x<<" y:"<<*y;
+}
+
+int main()
+{
+    cout<<"Before Swapping: ";
+    int a=10,b=20;   // Global Variables
+    cout<<"a:"<<a<<" b:"<<b;
+    
+    cout<<"\nAfter Swapping: ";
+    swap(&a,&b);       // Pass by address
+    cout<<" a:"<<a<<" b:"<<b;
+
+    return 0;
+}
